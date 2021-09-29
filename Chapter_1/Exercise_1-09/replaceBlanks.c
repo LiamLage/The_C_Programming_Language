85c3d274a5926e45db7cd426292b8e1b944ae64b
Liam Lage <44103555+LiamLage@users.noreply.github.com>
2021-09-29 18:38:57 +0200

    Add exercises

--- Chapter_1/Exercise_1-09/replaceBlanks.c
+++ Chapter_1/Exercise_1-09/replaceBlanks.c
@@ -4,18 +4,41 @@
  */
 #include <stdio.h>
 
+/*  copy input to output, replacing each  
+ *  sequence of one or more blanks with
+ *  a single blank.
+ * 
+ *  basic operation:
+ *  itterate through each character of the input 
+ *  test if the character is a blank
+ *      if the character is a blank, enter the 
+ *      function_if(), if its not a blank, goto
+ *      function_else()
+ *      
+ *      function_if()
+ *  	if blank flag HIGH, goto function_else()
+ *      if blank flag LOW, set blank flag HIGH, 
+ *      print a blank.
+ * 
+ *      function else()
+ *      set blank flag LOW
+ *      print the character
+ *      
+ */
+
 int main(void)  {
-    int c, isBlank;
-    isBlank = 0;
+    int c, blank_rx;
+    blank_rx = 0;
     printf("%s\n", "Enter characters, when complete press Return"); /* User prompt */
     while ((c = getchar()) != EOF)  {
-        if (c == ' ')   {
-            if (!isBlank)   {
-                isBlank = 1;
-                putchar(c);
+        if (c == ' ')   {        /* if its a ' ' enter body, if not; jump to else */
+            if (!blank_rx)   {   /* if blank_rx == 0 (blank_rx flag LOW), enter body.
+                                  * '!' operator reverses the logical state of blank_rx */  
+                blank_rx = 1;    /* blank_rx flag HIGH */
+                putchar(c);      /* print one ' ' */
             }
         } else {
-            isBlank = 0;
+            blank_rx = 0;       /*  blank_rx flag LOW */
             putchar(c);
         }
     }
---
+++ Chapter_1/Exercise_1-12/oneWordperLine.c
@@ -0,0 +1,20 @@
+/*  Author: Liam Lage
+ *  29/09/2021
+ *  Solutions for Exercise 1-09.
+ */
+#include <stdio.h>
+
+/* print input one word per line */
+
+int main(void)  {
+    int c;
+    printf("%s\n", "Enter characters, once complete press enter");
+
+    while ((c = getchar()) != EOF)  {
+        printf("%c", c);
+        if (c == ' ' || c == '\t' || c == '\n')  {
+            printf("\n");
+        }
+    }
+    return 0;
+}
