bfdd6d9250cd43283dc8e9ea323ce7d61f7e68ce
Liam Lage <44103555+LiamLage@users.noreply.github.com>
2021-10-03 01:16:11 +0200

    Add comments

--- Chapter_0/detect_temperature_conversion/detect_conversion.c
+++ Chapter_0/detect_temperature_conversion/detect_conversion.c
@@ -1,18 +1,24 @@
 /* Author: Liam Lage
  * 02/10/2021
  */
+
+/* Enter the temperature with the suffix C or F
+ * and press enter to convert from C to F and
+ * visa versa. */
 #include <stdio.h>
 #include <stdlib.h>
 #define MAXLENGTH 100
 
-float fahrenheit_to_celcius(float argFahrenheit)   {
+/* convert Fahrenheit to Celsius*/
+float fahrenheit_to_celsius(float argFahrenheit)   {
     float c = 0;
 
     c = (5.0/9.0)*(argFahrenheit - 32);
     return c;
 }
 
-float celcius_to_fahrenheit(float argCelsius)   {
+/* convert Celsius to Fahrenheit */
+float celsius_to_fahrenheit(float argCelsius)   {
     float f = 0;
 
     f = (argCelsius / (5.0/9.0)) + 32;
@@ -28,42 +34,33 @@ int get_string_length(char *str)   {
     return offset;
 }
 
-/* copy a string to another string */
-void copy_string(char *from, char *to)  {
-    int offset = 0;
-    while (from[offset] != 0)   {
-        to[offset] = from[offset];
-        offset++;
-    }
-    to[offset] = 0; // make sure it's NULL terminated!
-}
 
 int main(int argc, char **argv) {
-    int c, v, len, length;
-    char line[MAXLENGTH];
-    char num[MAXLENGTH] ;
-    len = 0;
-    length = 0;
+    int c, len;
+    float x;                            /* Holds the numerical value of num array */
+    char line[MAXLENGTH];               /* Holds the input line including suffix */
+    char num[MAXLENGTH] ;               /* Holds the input line with the suffix removed */
+    len = x = 0;
 
-    printf("%s\n\n", "Enter the temperature with the suffix C or F, then press Enter get a conversion:");
+    printf("%s\n\n", "Enter the temperature with the suffix C or F, then press Enter to convert:"); /* prompt the user */
 
-    for (int i = 0; (c = getchar()) != EOF && c != '\n'; ++i)  {
+    for (int i = 0; (c = getchar()) != EOF && c != '\n'; ++i)  {    /* save the input to an array*/
         line[i] = c;
     }
-    len = get_string_length(line);
+    len = get_string_length(line);                                  /* get the length of the input */
 
-    if (line[len - 3] == 'F')   {
-        for (int i = 0; i <= len - 3; ++i)
+    if (line[len - 1] == 'F')   {                                   /* the last letter is 'F', input is Fahrenheit */
+        for (int i = 0; i <= len - 1; ++i)
             num[i] = line[i];
-        float x = atof(num);
-        printf("%s\n%6.2f%s\n", "Fahrenheit to Celsius:", fahrenheit_to_celcius(x), "°C");
+        x = atof(num);                                              /* convert the string to a float */
+        printf("%s\n%6.2f%s\n", "Fahrenheit to Celsius:", fahrenheit_to_celsius(x), "°C");  /* convert from F to C and print */
     }
-    else if (line[len - 3] == 'C')  {
-        for (int i = 0; i <= len - 3; ++i)
+    else if (line[len - 1] == 'C')  {                               /* the last letter is 'C', input is Centigrade */
+        for (int i = 0; i <= len - 1; ++i)
             num[i] = line[i];
-        float y = atof(num);
-        printf("%s\n%6.2f%s\n", "Celsius to Fahrenheit:", celcius_to_fahrenheit(y), "°F");
+        x = atof(num);                                              /* convert the string to a float */
+        printf("%s\n%6.2f%s\n", "Celsius to Fahrenheit:", celsius_to_fahrenheit(x), "°F");  /* convert from C to F and print */
     }
     else
-        printf("ERROR");
+        printf("ERROR");                                            /* something went wrong */
 }
