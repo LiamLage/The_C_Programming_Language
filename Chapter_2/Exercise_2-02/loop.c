b76e9c31ff824ed50c5334c0c530b3ba3c420af8
Liam Lage <44103555+LiamLage@users.noreply.github.com>
2021-10-08 17:50:12 +0200

    Add exercise 2.2 & 2.3

---
+++ Chapter_2/Exercise_2-02/loop.c
@@ -0,0 +1,40 @@
+/* Author: Liam Lage
+ * 07/10/2021
+ *
+ * Solution for Exercise 2-2.
+ */
+#include <stdio.h>
+#include <stdint.h>
+#define MAX_SIZE 1000
+
+ /* for (i = 0; i < lim - 1 && (c = getchar()) != '\n' && c != EOF; ++i)
+  *     s[i] = c;
+  * write a loop equivalent to the for loop above without using && or ||. */
+
+    /* before reading a new character it is necesary to check
+     * that there is room to store it in the array s[], so the
+     * test i < LIM - 1 must be done first. If this test fails
+     * we must not go on to read another character, similarly
+     * the call and assignment of c must occur before it is
+     * tested against EOF. */
+
+ int main(void) {
+     int32_t c, i, lim;
+     char s[MAX_SIZE];
+     i = 0;
+
+     printf("Enter a string, then press Enter to proceed:\n");  // prompt user
+     while (i < MAX_SIZE - 1) {
+        if ((c = getchar()) == '\n')
+            break;
+        if (c == EOF)
+            break;
+        else
+            s[i++] = c;
+     }
+     s[i] = '\0';   // NULL terminate
+
+     printf("\n%s\n%s\n", "Result:", s);    // print string s[]
+
+     return 0;
+ }
---
+++ Chapter_2/Exercise_2-03/htoi.c
@@ -0,0 +1,70 @@
+/* Author: Liam Lage
+ * 08/10/2021
+ *
+ * Solution for Exercise 2-3.
+ */
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include <ctype.h>
+
+#define LIM 1000
+
+int32_t usr_input(char s[], int32_t lim);
+uint32_t htoi(char s[], int32_t len);
+
+int main(void)  {
+    char line[LIM];
+    int32_t len;
+
+    while ((len = usr_input(line, LIM)) > 0) {
+        printf("Hexidecimal Entered: %s\n", line);
+        printf("Decimal Result:      %lld\n", htoi(line, len));
+        printf("\nPress Ctrl+Z to exit.\n\n\n");
+    }
+    return 0;
+}
+
+int32_t usr_input(char s[], int32_t lim) {
+    int32_t c, i;
+
+    printf("Enter a Hexidecimal number with the suffix 0x or 0X (optional):\n");    // prompt user
+    for (i = 0; i < lim - 1 && (c = getchar()) != '\n' && c != EOF; ++i)
+        s[i] = c;
+    s[i] = '\0';
+    return i - 1;   // return length of s[] - '\0'
+}
+
+uint32_t htoi(char s[], int32_t len) {
+    int32_t i,c, n;
+    long long dec;
+    n = dec = 0;
+    /* decimal calculation steps:
+     * Chapter_2\Exercise_2-03\hextoint.jpg
+     * decimal = d    x 16^n-1 + d    x 16^n-1
+     *            n-1             n-1
+     */
+    
+    for (i = 0; s[i] != '\0'; ++i) {
+        c = s[i];
+        // Allow optional 0x or 0X suffix
+        if (i == 0 && c == '0' && (s[1] == 'x' || s[1] == 'X')) { 
+            len -= 2;
+            i = 1;
+            continue;
+        }
+        /* Find decimal representation of hexidecimal 
+         * (an ASCII chart is useful to visualize this) */
+        if (isdigit(c))
+            n = c - '0';        // '0' = Dec 48 ASCII
+        else if (islower(c))
+            n = c - 'a' + 10;   // 'a' = Dec 97 ASCII
+        else if (isupper(c))
+            n = c - 'A' + 10;   // 'A' = Dec 65 ASCII  
+        
+        dec += n * pow(16, len); 
+        len--;
+    }
+    return dec;   
+}
+    
